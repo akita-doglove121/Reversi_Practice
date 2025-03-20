8d8a45b54635622b3b02a52aebbfe3bee91fefef
Karin_121 <work.kt73study121@gmail.com>
2025-03-20 21:56:47 +0900

    改善
    コマが置けない場合の処理を追加
    
    引き分けの場合の処理を追加
    
    無効な入力があった場合再度促しプレイヤーが交代しないようにした

--- Osero/src/reversi/ReversiMain.java
+++ Osero/src/reversi/ReversiMain.java
@@ -1,45 +1,45 @@
 package reversi;
+
 import java.util.*;
 
 public class ReversiMain {
-    public static void main(String[] args) {
-        ReversiBoard board = new ReversiBoard(); // 盤面オブジェクト
-        GameSystem gameSystem = new GameSystem(board); // GameSystem に盤面を渡す
-        ReversiCell currentPlayer = ReversiCell.BLACK;
+	public static void main(String[] args) {
+		ReversiBoard board = new ReversiBoard(); // 盤面オブジェクト
+		Scanner scanner = new Scanner(System.in);
+		GameSystem gameSystem = new GameSystem(board, scanner); // GameSystem に盤面を渡す
+		ReversiCell currentPlayer = ReversiCell.BLACK;
 
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            // 最新の盤面を表示
-            board.displayBoard();
-
-            // ユーザーの入力
-            System.out.println(currentPlayer + " あなたのターンです。コマを置いてください (行 列, 1 から指定):");
-            int row = scanner.nextInt() - 1; // 1 ベースを 0 ベースに変換
-            int col = scanner.nextInt() - 1;
-
-            // 無効な手の場合は再入力を促す
-            if (!gameSystem.canPlacePiece(row, col, currentPlayer)) {
-                System.out.println("そこには置けません。もう一度入力してください。");
-                continue;
-            }
-
-            // 勝敗のチェック
-            if (gameSystem.isGameOver()) {
-                board.displayBoard();
-                ReversiCell winner = gameSystem.checkWinner();
-                if (winner == ReversiCell.EMPTY) {
-                    System.out.println("引き分けです！");
-                } else {
-                    System.out.println("勝者: " + winner);
-                }
-                break;
-            }
-
-            // プレイヤー交代
-            currentPlayer = gameSystem.switchPlayer(currentPlayer);
-        }
-
-        scanner.close();
-    }
+		while (true) {
+			board.displayBoard(); // 最新の盤面を表示
+			// 現在のプレイヤーがコマを置けるかどうか確認
+			if (!gameSystem.canPlayerMove(currentPlayer)) {
+				System.out.println(currentPlayer + "はコマを置けません。パスします。");
+				currentPlayer = gameSystem.switchPlayer(currentPlayer);
+				continue;
+			}
+			// コマを置ける場合にターンを開始
+			System.out.println(currentPlayer + "のターンです。");
+			// 有力な入力を取得する
+			int[] position = gameSystem.getValidInput(currentPlayer);
+			int row = position[0];
+			int col = position[1];
+			// 駒を置いて反転処理を行う
+			if (!gameSystem.placeStone(row, col, currentPlayer)) {
+				System.out.println("その場所にはコマを置けません。再度入力してください。");
+				continue; // 無効な入力があった場合は再度入力を促す
+			}
+			if (gameSystem.isGameOver()) {// 勝敗のチェック
+				board.displayBoard();
+				ReversiCell winner = gameSystem.checkWinner();
+				if (winner == ReversiCell.EMPTY) {
+					System.out.println("引き分けです！");
+				} else {
+					System.out.println("勝者: " + winner);
+				}
+				break;
+			}
+			// プレイヤー交代
+			currentPlayer = gameSystem.switchPlayer(currentPlayer);
+		}
+	}
 }
